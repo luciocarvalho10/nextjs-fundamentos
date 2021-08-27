b467d4c13ce1da5491e0ddb97fe6841722488172
Lucio <luciocarvalho10@hotmail.com.br>
2021-08-27 18:39:15 -0400

    bugs de build

--- src/components/Navegador.jsx
+++ src/components/Navegador.jsx
@@ -4,7 +4,7 @@ import styles from '../styles/Navegador.module.css'
 export default function Navegador(props) {
   return (
     <div className={styles.link}>
-      <Link href={props.destino}>
+      <Link href={props.destino} passHref>
         {props.texto}
       </Link>
     </div>
